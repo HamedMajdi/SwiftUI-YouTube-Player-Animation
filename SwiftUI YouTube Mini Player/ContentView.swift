c19d03dd4e8bd94ba9ebb09be60167d243246b3c
hamedmajdi <hamed90_viva@yahoo.com>
2024-02-24 08:11:28 +0300

    Initial Commit

---
+++ SwiftUI YouTube Mini Player/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI YouTube Mini Player
+//
+//  Created by Hamed Majdi on 2/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI YouTube Mini Player/SwiftUI_YouTube_Mini_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_YouTube_Mini_PlayerApp.swift
+//  SwiftUI YouTube Mini Player
+//
+//  Created by Hamed Majdi on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_YouTube_Mini_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
