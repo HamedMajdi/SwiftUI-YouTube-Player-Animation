8a92e7e3f58eb283602746d2623456f8414b75f6
hamedmajdi <hamed90_viva@yahoo.com>
2024-02-24 23:00:10 +0300

    First Commit

---
+++ SwiftUI YouTube Mini Player/Model/Tab.swift
@@ -0,0 +1,18 @@
+//
+//  Tab.swift
+//  SwiftUI YouTube Mini Player
+//
+//  Created by Hamed Majdi on 2/24/24.
+//
+
+import SwiftUI
+
+struct Tab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Tab()
+}
---
+++ SwiftUI YouTube Mini Player/View/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  SwiftUI YouTube Mini Player
+//
+//  Created by Hamed Majdi on 2/24/24.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
