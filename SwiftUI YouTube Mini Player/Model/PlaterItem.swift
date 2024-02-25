2c850abfab0c7829507dc8bc0966f3631203c7bd
hamedmajdi <hamed90_viva@yahoo.com>
2024-02-25 09:17:20 +0300

    Completed Home View

---
+++ SwiftUI YouTube Mini Player/Model/PlaterItem.swift
@@ -0,0 +1,18 @@
+//
+//  PlaterItem.swift
+//  SwiftUI YouTube Mini Player
+//
+//  Created by Hamed Majdi on 2/24/24.
+//
+
+import SwiftUI
+
+struct PlaterItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlaterItem()
+}
