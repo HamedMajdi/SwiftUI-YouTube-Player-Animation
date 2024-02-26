585e2446da0cad9cb97606847d9e873ee305ade1
hamedmajdi <hamed90_viva@yahoo.com>
2024-02-26 20:45:43 +0300

    wrote the code for MiniPlayerView

---
+++ SwiftUI YouTube Mini Player/View/MiniPlayerView.swift
@@ -0,0 +1,18 @@
+//
+//  MiniPlayerView.swift
+//  SwiftUI YouTube Mini Player
+//
+//  Created by Hamed Majdi on 2/26/24.
+//
+
+import SwiftUI
+
+struct MiniPlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MiniPlayerView()
+}
